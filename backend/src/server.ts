8a3c68a78ac206ee89e85f6c2cecb976bcab1a85
Weslley Barros <weslley17w@gmail.com>
2025-02-02 15:25:50 -0300

    Corrige porta defalt para render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import cors from "cors"
 import { app } from "./app"
 
-const port = process.env.SERVER_PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.listen(port, () => console.log(`Server is running on port ${port}`))
