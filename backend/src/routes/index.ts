774700b13b0021bcac2cdf1ebdc489f3cbcfee26
Caio Almeida <caioantonio0903@gmail.com>
2025-02-09 01:05:26 -0300

    creates backbone for rentals

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express"
 import homeRoutes from "./homeRouters"
 import userRoutes from "./userRouters"
 import productRouters from "./productRouters"
+import rentalRouters from "./rentalRouters"
 
 const router = Router()
 
 router.use("/", homeRoutes)
 router.use("/user", userRoutes)
 router.use("/products", productRouters)
+router.use("/rentals", rentalRouters)
 
 export { router }
